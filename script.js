57fe76b36d0268f492822e75739563bf521b1761
anujp141 <134326334+anujp141@users.noreply.github.com>
2023-09-05 03:20:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let squares = Array(9).fill(null);
-let xIsNext = true; 
-let winner = null; 
+// JavaScript (script.js)
+
+// Initialize the game state
+let squares = Array(9).fill(null); // Represents the game board
+let xIsNext = true; // Track the current player (X or O)
+let winner = null; // Track the winner (X, O, or null for a draw)
 
 // Function to handle square click events
 function handleClick(index) {
@@ -12,6 +15,7 @@ function handleClick(index) {
     winner = calculateWinner();
     if (winner) {
         document.getElementById('game-status').textContent = `Winner: ${winner}`;
+        highlightWinningSquares(winner);
     } else if (!squares.includes(null)) {
         document.getElementById('game-status').textContent = "It's a draw!";
     } else {
@@ -40,6 +44,28 @@ function calculateWinner() {
     return null; // No winner yet
 }
 
+// Function to highlight the winning squares
+function highlightWinningSquares(winner) {
+    const winningCombos = calculateWinningCombos(winner);
+
+    for (const combo of winningCombos) {
+        for (const index of combo) {
+            document.getElementsByClassName('square')[index].classList.add('winning-square');
+        }
+    }
+}
+
+// Function to calculate the winning combos
+function calculateWinningCombos(winner) {
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+
+    return winningCombos.filter(combo => combo.every(index => squares[index] === winner));
+}
+
 // Attach click event handlers to each square
 const squareElements = document.getElementsByClassName('square');
 for (let i = 0; i < squareElements.length; i++) {
@@ -54,5 +80,6 @@ document.getElementById('reset-button').addEventListener('click', () => {
     document.getElementById('game-status').textContent = `Next player: X`;
     for (const square of squareElements) {
         square.textContent = '';
+        square.classList.remove('winning-square');
     }
 });
